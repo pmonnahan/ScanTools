d230e7e9e9f846a92b989e2521fdb3b7c91d8a91
Patrick Monnahan <pmonnahan@gmail.com>
2017-06-14 13:58:11 +0100

    adds suffix option to calcFreqs_atSites.py

--- calcFreqs_atSites.py
+++ calcFreqs_atSites.py
@@ -1,7 +1,7 @@
 import argparse
 
 
-def calcFreqs(input_directory, output_directory, output_filename, sites_file, pops):
+def calcFreqs(input_directory, output_directory, output_filename, sites_file, pops, suffix):
     if input_directory.endswith("/") is False:
         input_directory += "/"
     if output_directory.endswith("/") is False:
@@ -21,7 +21,7 @@ def calcFreqs(input_directory, output_directory, output_filename, sites_file, po
     print(pop_list)
 
     for pop in pop_list:
-        with open(input_directory + pop + ".table.repol.txt") as pop_data:
+        with open(input_directory + pop + suffix) as pop_data:
             for line in pop_data:
                 line = line.strip("\n").strip("\t").split("\t")
                 scaff = int(line[2].split("_")[1])
@@ -47,6 +47,7 @@ if __name__ == '__main__':  # Used to run code from command line
     parser.add_argument('-of', type=str, metavar='output_filename', required=True, help='Output Filename')
     parser.add_argument('-s', type=str, metavar='sites_file', required=True, help='full absolute path to file containing scaffold and position for each site')
     parser.add_argument('-pops', type=str, metavar='populations', required=True, help='Size of windows in bp')
+    parser.add_argument('-suffix', type=str, metavar='pop_data_suffx', required=True, help='suffix of file containing population data')
     args = parser.parse_args()
 
-    j1 = calcFreqs(args.i, args.o, args.of, args.s, args.pops)
+    j1 = calcFreqs(args.i, args.o, args.of, args.s, args.pops, args.suffix)
